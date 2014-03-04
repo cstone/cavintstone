7a78c7e276aeebca0fcd54ec219f0d5f5dfcfdcd
Cavin <cstone2003@hotmail.com>
2014-03-03 18:54:43 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cavintstone::Application.config.secret_token = '3c2fceccb3a449e1dbb1216a7a9a7c3262a8fdc0fcf2eeac6fe77314718697b9b020f02e2a0a9d2b4c50f827ef8ed4b74affb7abcef8e5cccafe5898cbb6e802'
