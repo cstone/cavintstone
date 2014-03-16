2664cb0395a898f00068b8c1cc846d450640bb3c
Cavin <cstone2003@hotmail.com>
2014-03-16 15:52:24 -0500

    Add articles to ActiveAdmin

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -1,3 +1,10 @@
 ActiveAdmin.register Article do
-  
+  index do
+    selectable_column
+    column :title
+    column :body
+    column :published
+    column :published_at
+    default_actions
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
-  attr_accessible :body, :published, :title
+  attr_accessible :body, :published, :title, :published_at
 
   default_scope order('published_at DESC')
+  scope :published, where(published: true)
+  scope :draft, where(published: false)
 end
