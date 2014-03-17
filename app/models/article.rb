3ffca9058e47a35d6b38aab1d5c4390a0c6b3e35
Cavin <cstone2003@hotmail.com>
2014-03-16 21:25:18 -0500

    Add resume carrierwave

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,9 @@ class Article < ActiveRecord::Base
   attr_accessible :body, :published, :title, :published_at
 
   default_scope order('published_at DESC')
+
   scope :published, where(published: true)
   scope :draft, where(published: false)
+
+
 end
