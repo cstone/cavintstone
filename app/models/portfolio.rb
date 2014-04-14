8c44648b4d53395c90df7fa50f0a33e322ca1c98
Cavin <cstone2003@hotmail.com>
2014-04-14 18:30:41 -0500

    Add to_param method to portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,12 @@
 class Portfolio < ActiveRecord::Base
-  attr_accessible :description, :portfolio_image, :title, :website
+  attr_accessible :description, :portfolio_image, :title, :website, :portfolio_image_cache, :remove_portfolio_image, :portfolio_category_id, :technology_ids
+
+  mount_uploader :portfolio_image, PortfolioImageUploader
+
+  has_and_belongs_to_many :technologies
+  belongs_to :portfolio_category
+
+  def to_param
+    "#{id}-#{title.parameterize}"
+  end
 end
