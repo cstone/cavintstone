36bed996480e7a4e3de0283af7251a07adf7398c
Cavin <cstone2003@hotmail.com>
2014-04-14 19:36:57 -0500

    Remove description from categories migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140413142212) do
+ActiveRecord::Schema.define(:version => 20140415003433) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -58,9 +58,8 @@ ActiveRecord::Schema.define(:version => 20140413142212) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "dynamic_contents", :force => true do |t|
