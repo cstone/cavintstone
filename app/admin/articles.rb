e7a5775bb7ed8e1e2cef8c6b1c7d429dbf2b0b77
Cavin <cstone2003@hotmail.com>
2014-04-14 19:31:14 -0500

    Update index, show and form on ActiveAdmin

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -1,15 +1,63 @@
 ActiveAdmin.register Article do
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :published
+      f.input :published_at
+      f.input :body, as: :html_editor
+      f.input :article_image, :as => :file, :hint => f.object.article_image.present? \
+    ? f.template.image_tag(f.object.article_image.url(:thumb))
+      : f.template.content_tag(:span, "no photo yet")
+      f.input :article_image_cache, :as => :hidden
+      f.input :remove_article_image, as: :boolean, label: 'Check here to remove the current image.'
+    end
+    f.actions
+  end
+
+
+
   index do
     selectable_column
     column :article_image do |article|
-      if article.article_image
+      if !article.article_image.empty?
         image_tag article.article_image.url(:thumb)
       end
     end
     column :title
-    column :body
+    column :body do |body|
+      body.body.truncate(75).html_safe
+    end
     column :published
     column :published_at
     default_actions
   end
+
+
+  show do |article|
+    attributes_table do
+      row :id
+      row :title
+      row :published
+      row :published_at
+      row :body do
+        article.body.html_safe
+      end
+      row :article_image do
+        if !article.article_image.empty?
+          image_tag article.article_image.url
+        end
+      end
+      row :created_at do
+        article.created_at.to_formatted_s(:default)
+      end
+      row :updated_at do
+        article.updated_at.to_formatted_s(:default)
+      end
+    end
+    active_admin_comments
+  end
+
+
+
 end
