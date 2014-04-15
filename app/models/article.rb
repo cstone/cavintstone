f8da68d01fa973834309a67f7e2dc99af0a724c9
Cavin <cstone2003@hotmail.com>
2014-04-14 19:30:39 -0500

    Add article_image_uploader

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,11 @@
 class Article < ActiveRecord::Base
-  attr_accessible :body, :published, :title, :published_at, :article_image
+  attr_accessible :body, :published, :title, :published_at, :article_image, :remove_article_image, :article_image_cache
 
   default_scope order('published_at DESC')
 
+  mount_uploader :article_image, ArticleImageUploader
+
   scope :published, where(published: true)
   scope :draft, where(published: false)
-
 
 end
