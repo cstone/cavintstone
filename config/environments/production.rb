5b7adf9e22b921f524978826c4d93b657bc502eb
Cavin <cstone2003@hotmail.com>
2014-04-21 20:00:34 -0400

    Add "config.assets.precompile += %w( mystyles.css )" to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Cavintstone::Application.configure do
+
+  config.assets.precompile += %w( mystyles.css )
+
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
