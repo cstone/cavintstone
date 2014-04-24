0e5e471bc5f67ee60f4802745568a59063509aad
Cavin <cstone2003@hotmail.com>
2014-04-24 13:57:08 -0500

    Add @contact_title instance variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
     @home_name = DynamicContent.get_value(:home_name)
     @home_job_title = DynamicContent.get_value(:home_job_title)
     @home_summary = DynamicContent.get_value(:home_summary)
+    @contact_title = DynamicContent.get_title(:contact_title)
     @contact_content = DynamicContent.get_value(:contact_content)
     @resume_link = Resume.first
     @all_articles = Article.all
